a48d7273bca082771609db66baf65ac7b6b1aa51
Reclaimed1 <112954533+Reclaimed1@users.noreply.github.com>
2025-07-08 16:34:05 +0100

    Update MakeLifeEasy.jsx

--- src/cmp/MakeLifeEasy.jsx
+++ src/cmp/MakeLifeEasy.jsx
@@ -5,7 +5,7 @@ export default function MakeLifeEasy() {
             <div className="text-start text-xl font-bold flex bg-[#175046] text-white py-9 px-5 md:mx-20 md:py-11 md:text-4xl md:justify-between md:px-40 md:item-center">
             <h1>We're Here to Make Yor Life Easier</h1>
             <div>
-            <a href="/Loading" className="bg-[#5e7d3f] py-2 px-4 text-sm ml-3 hover:bg-gradient-to-b from-white via-[#5e7d3f] to-[#5e7d3f] rounded-md" >BOOK NOW</a>
+            <a href="/Loading" className="bg-[#5e7d3f] py-2 px-4 text-sm ml-3 hover:bg-gradient-to-b from-white via-[#5e7d3f] to-[#5e7d3f] rounded-md" >BOOK</a>
             </div>
           </div>
         </div>
